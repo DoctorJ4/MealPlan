dc503d2c939c0361ee2797d87eaeaad8f0fa804f
totalwar235 <rbnorstrom@pluto.dsu.edu>
2015-04-14 21:53:52 -0500

    added new activity

--- app/src/main/java/doctorj/mealplan/MainActivity.java
+++ app/src/main/java/doctorj/mealplan/MainActivity.java
@@ -47,9 +47,10 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        //startActivity(Display_Details);
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
 
